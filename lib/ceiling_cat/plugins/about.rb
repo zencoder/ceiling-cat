f4507f5d672035be13bfc27a523a58da72958632
Christopher Warren <christopher.warren@gmail.com>
2011-08-13 13:27:09 -0700

    Removed note storage tests

--- lib/ceiling_cat/plugins/about.rb
+++ lib/ceiling_cat/plugins/about.rb
@@ -4,8 +4,6 @@ module CeilingCat
       def self.commands
         [{:regex => /^!plugins/i, :name => "!plugins", :description => "List of installed plugins.", :method => "list_plugins"},
          {:regex => /^!commands/i, :name => "!commands", :description => "List of available commands.", :method => "list_commands"},
-         {:regex => /^!savenote/i, :name => "!savenote", :description => "Save a note.", :method => "savenote"},
-         {:regex => /^!loadnote/i, :name => "!loadnote", :description => "Save a note.", :method => "loadnote"},
          {:regex => /^!employees/i, :name => "!employees", :description => "List of employees in the room.", :method => "list_employees"}]
       end
 
@@ -32,15 +30,6 @@ module CeilingCat
       def list_employees
         members = room.list_of_users_in_room(:type => "member")
         reply "#{members} #{pluralize(members.size, "is a", "are")} Zencoder #{pluralize(members.size, "employee")}."
-      end
-      
-      def savenote
-        store["note"] = body_without_command(/^!savenote /)
-        reply "Note saved!"
-      end
-      
-      def loadnote
-        reply store["note"] || "There was no note..."
       end
     end
   end
